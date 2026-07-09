1d76d1419f9e7556edc08c68aece2af2347d5f8f
Matthew D Wilson <mattrick2214@gmail.com>
2026-07-08 22:01:38 -0500

    added slice reducers and actions boilerplate code from 'https://redux.js.org/tutorials/essentials/part-2-app-structure' as instructed by Udemy insctrutor Ivan Lourenço Gomes

---
+++ src/store/booksSlice.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+// Slices contain Redux reducer logic for updating state, and
+// generate actions that can be dispatched to trigger those updates.
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    increment: state => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1
+    },
+    decrement: state => {
+      state.value -= 1
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    }
+  }
+})
+
+// Export the generated action creators for use in components
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+// Export the slice reducer for use in the store configuration
+export default counterSlice.reducer
